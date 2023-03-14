227c96d1377ca062eded35c2fa72cb5eee30bf77
msaeva <mariq2121@gmail.com>
2023-03-14 23:18:46 +0200

    added new view for search with pagination and refactored update song

--- src/main/java/bg/softuni/mmusic/controllers/validations/PublicSongValidation.java
+++ src/main/java/bg/softuni/mmusic/controllers/validations/PublicSongValidation.java
@@ -3,13 +3,8 @@ package bg.softuni.mmusic.controllers.validations;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
-public class SearchSongValidation extends Pageable {
-
-    private List<String> sort;
-
+public class PublicSongValidation extends Pageable {
     private String style;
 }
